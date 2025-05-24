4f0058ded6f7582bd8633e91a6b8bcbc7de66166
ased220 <zvp2005@gmail.com>
2025-05-24 11:13:00 +0300

    fixed filter fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { filterFetch } from "./API";
 
 
 function App() {
 
+  filterFetch('completed')
+  .then(response => {
+    console.log('Total amount:', response);
+  })
+  .catch(error => console.error('Ошибка запроса:', error));
   return (
     <>
 
